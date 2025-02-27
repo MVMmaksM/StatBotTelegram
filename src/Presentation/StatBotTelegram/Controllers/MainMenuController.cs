84b130f27f12dc73fb89fe8606c163075e53e4aa
MVMmaksM <vaganov.maksim94@yandex.ru>
2025-02-27 17:10:41 +0500

    refactor maincontroller

--- src/Presentation/StatBotTelegram/Controllers/MainMenuController.cs
+++ src/Presentation/StatBotTelegram/Controllers/MainMenuController.cs
@@ -4,6 +4,7 @@ using StatBotTelegram.Components;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace StatBotTelegram.Controllers;
 
@@ -11,48 +12,42 @@ public class MainMenuController(ITelegramBotClient botClient, IStateUser stateUs
 {
     public async Task Handle(Message message, CancellationToken cancellationToken)
     {
+        var textMessage = string.Empty;
+        KeyboardButton[][] buttonMenu = null;
+        
+        //в зависимости от выбранной команды
+        //устанавливаем сообщение, кнопки и состояние
         switch (message.Text)
         {
             case "/start":
                 //устанавливаем состояние
                 stateUser.SetStateMenu(message.Chat.Id, MenuItems.MainMenu);
-                //отправляем ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true, replyParameters: message.Id,
-                    text: $"Добро пожаловать, {message.From.FirstName}!\n\n{ConstTextMessage.WelcomeText}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsMainMenu);
+                textMessage = $"Добро пожаловать, {message.From.FirstName}!\n\n{ConstTextMessage.WelcomeText}";
+                buttonMenu = KeyboradButtonMenu.ButtonsMainMenu;
                 break;
             case "Поиск специалиста, ответственного за форму":
                 //устанавливаем состояние
                 stateUser.SetStateMenu(message.Chat.Id, MenuItems.SearchEmployees);
-                //отправляем ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true,
-                    replyParameters: message.Id,
-                    text: $"{ConstTextMessage.SearchEmployees}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsSearchEmployeesMenu);
+                textMessage = ConstTextMessage.SearchEmployees;
+                buttonMenu = KeyboradButtonMenu.ButtonsSearchEmployeesMenu;
                 break;
             case "Получение данных о кодах статистики и перечня форм":
                 //устанавливаем состояние
                 stateUser.SetStateMenu(message.Chat.Id, MenuItems.GetInfoCodesAndListForm);
-                //отправляем ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true,
-                    replyParameters: message.Id,
-                    text: $"{ConstTextMessage.SelectCommand}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsInfoCodesAndListForm);
+                textMessage = ConstTextMessage.SelectCommand;
+                buttonMenu = KeyboradButtonMenu.ButtonsInfoCodesAndListForm;
                 break;
             default:
-                //отправляем ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true, replyParameters: message.Id,
-                    text: $"{ConstTextMessage.UnknownCommand}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsMainMenu);
+                textMessage = ConstTextMessage.UnknownCommand;
+                buttonMenu = KeyboradButtonMenu.ButtonsMainMenu;
                 break;
         }
+        
+        //отправляем ответ
+        await botClient.SendMessage(chatId: message.Chat.Id, 
+            protectContent: true, replyParameters: message.Id,
+            text: textMessage,
+            parseMode: ParseMode.Html, cancellationToken: cancellationToken,
+            replyMarkup: buttonMenu);
     }
 }
