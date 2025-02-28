6a018284e0e102f45d3b84d7388d49be5fe4a425
MVMmaksM <vaganov.maksim@mail.ru>
2025-02-28 09:54:12 +0500

    refactor InfoMainMenuController

--- src/Presentation/StatBotTelegram/Controllers/Info/InfoMainMenuController.cs
+++ src/Presentation/StatBotTelegram/Controllers/Info/InfoMainMenuController.cs
@@ -4,6 +4,7 @@ using StatBotTelegram.Components;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace StatBotTelegram.Controllers;
 
@@ -11,48 +12,42 @@ public class InfoMainMenuController(ITelegramBotClient botClient, IStateUser sta
 {
     public async Task Handle(Message message, CancellationToken cancellationToken)
     {
+        var textMessage = string.Empty;
+        KeyboardButton[][] buttonMenu = null;
+        
         switch (message.Text)
         {
             case "Получить данные о кодах статистики организации":
-                //ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true, replyParameters: message.Id,
-                    text: $"{ConstTextMessage.SelectCommand}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsSearchOkpoInnOgrn);
+                textMessage = ConstTextMessage.SelectCommand;
+                buttonMenu = KeyboradButtonMenu.ButtonsSearchOkpoInnOgrn;
                 //устанавливаем сосотояние
                 stateUser.SetStateMenu(message.Chat.Id, MenuItems.GetInfoOrganization);
                 break;
             case "Получить перечень форм":
-                //ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true, replyParameters: message.Id,
-                    text: $"{ConstTextMessage.SelectCommand}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsSearchOkpoInnOgrn);
+                textMessage = ConstTextMessage.SelectCommand;
+                buttonMenu = KeyboradButtonMenu.ButtonsSearchOkpoInnOgrn;
                 //устанавливаем сосотояние
                 stateUser.SetStateMenu(message.Chat.Id, MenuItems.GetListForm);
                 break;
             case "Назад":
-                //ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true, replyParameters: message.Id,
-                    text: $"{ConstTextMessage.SelectCommand}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsMainMenu);
+                textMessage = ConstTextMessage.SelectCommand;
+                buttonMenu = KeyboradButtonMenu.ButtonsMainMenu;
                 //устанавливаем сосотояние
                 stateUser.SetStateMenu(message.Chat.Id, MenuItems.MainMenu);
                 break;
             default:
-                //ответ
-                await botClient.SendMessage(chatId: message.Chat.Id, 
-                    protectContent: true, replyParameters: message.Id,
-                    text: $"{ConstTextMessage.UnknownCommand}",
-                    parseMode: ParseMode.Html, cancellationToken: cancellationToken,
-                    replyMarkup: KeyboradButtonMenu.ButtonsInfoCodesAndListForm);
+                textMessage = ConstTextMessage.UnknownCommand;
+                buttonMenu = KeyboradButtonMenu.ButtonsInfoCodesAndListForm;
                 //сбрасываем состояние команды
                 stateUser.RemoveOperationCode(message.Chat.Id);
                 break;
         }
+        
+        //ответ
+        await botClient.SendMessage(chatId: message.Chat.Id, 
+            protectContent: true, replyParameters: message.Id,
+            text: textMessage,
+            parseMode: ParseMode.Html, cancellationToken: cancellationToken,
+            replyMarkup: buttonMenu);
     }
 }
